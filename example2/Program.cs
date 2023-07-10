c5e0720ab7066782fbe551efc6dc4cf2fea45792
NikoRino <niko89rino@gmail.com>
2023-07-10 18:45:25 +0700

    Ввел программу сравнения

--- example2/Program.cs
+++ example2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+    Console.WriteLine("Первое число больше");
+else
+{
+    Console.WriteLine("Второе число больше");
+}
